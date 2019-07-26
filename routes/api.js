0bde0f7ae43ec94d5b738baf1aedd1121d281374
alexis trabajo <edwinalexis1996@gmail.com>
2019-07-26 11:31:08 -0500

    clase 12

--- routes/api.js
+++ routes/api.js
@@ -1,26 +1,34 @@
 const API_PATH = '/api';
 const films = require("../resources/films.json");
 const mongo = require("../db/connect");
-const { postFilm, getFilms, getFilmByID } = require("../db");
+const { postFilm, getFilms, getFilmByID, deleteFilmById } = require("../db");
 
 module.exports = (app) => {
-    app.get(`${API_PATH}/films`, async (req, res)=>{
+    app.get(`${API_PATH}/films`, async (req, res) => {
         const resp = await getFilms();
         return res.json(resp);
     });
 
-    app.get(`${API_PATH}/films/:id`, async (req, res)=>{
+    app.get(`${API_PATH}/films/:id`, async (req, res) => {
         const id = req.params.id;
         const resp = await getFilmByID(id);
         return res.json(resp);
     });
 
-    app.post(`${API_PATH}/film`, async (req, res)=>{
+    app.post(`${API_PATH}/film`, async (req, res) => {
         const film = JSON.parse(req.body.film);
-        if (film) { 
+        if (film) {
             const resp = await postFilm(film);
             return res.json(resp);
         }
         res.status(400).send({ reason: "No film sent." });
+    });
+    app.delete(`${API_PATH}/films/:id`, async (req, res) => {
+        const id = req.params.id;
+        if (id) {
+            const response = await deleteFilmById(id)
+            return res.json(response);
+        }
+        res.status(400).send({ reason: "No id sent." });
     })
 }
